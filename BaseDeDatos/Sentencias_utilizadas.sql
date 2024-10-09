f03a57d2d458e0b113dc9df9c272f7a9363746ec
Enzo Fabian Lopez <enzolopez789@gmail.com>
2024-10-09 10:31:27 -0300

    conexion con notebook

--- BaseDeDatos/Sentencias_utilizadas.sql
+++ BaseDeDatos/Sentencias_utilizadas.sql
@@ -1,37 +1,37 @@
-SELECT * FROM Usuario
+--SELECT * FROM Usuario
 
-SELECT * FROM Rol
+--SELECT * FROM Rol
 
-INSERT INTO Rol(descripcion)
-VALUES('Administrador')
+--INSERT INTO Rol(descripcion)
+--VALUES('Administrador')
 
-INSERT INTO Usuario(DNI,Nombre_completo,Email,Pass,telefono,direccion,estado,id_rol)
-VALUES ('40404040','Enzo','admin@gmail.com','123','3794111111','calle123',1,1)
+--INSERT INTO Usuario(DNI,Nombre_completo,Email,Pass,telefono,direccion,estado,id_rol)
+--VALUES ('40404040','Enzo','admin@gmail.com','123','3794111111','calle123',1,1)
 
-INSERT INTO Usuario(DNI,Nombre_completo,Email,Pass,telefono,direccion,estado,id_rol)
-VALUES ('20202020','Damian','vende@gmail.com','123','3794222222','calle456',1,2)
+--INSERT INTO Usuario(DNI,Nombre_completo,Email,Pass,telefono,direccion,estado,id_rol)
+--VALUES ('20202020','Damian','vende@gmail.com','123','3794222222','calle456',1,2)
 
-INSERT INTO Usuario(DNI,Nombre_completo,Email,Pass,telefono,direccion,estado,id_rol)
-VALUES ('10101010','Fabian','super@gmail.com','123','3794333333','calle789',1,3)
+--INSERT INTO Usuario(DNI,Nombre_completo,Email,Pass,telefono,direccion,estado,id_rol)
+--VALUES ('10101010','Fabian','super@gmail.com','123','3794333333','calle789',1,3)
 
-INSERT INTO Rol(descripcion)
-VALUES('Vendedor')
-
-
-INSERT INTO Rol(descripcion)
-VALUES('Supervisor')
+--INSERT INTO Rol(descripcion)
+--VALUES('Vendedor')
 
 
---consulta para llevar todos los usuario a la cd_usuarios--
-select u.id_usuario,u.DNI,u.Nombre_completo,u.Email,u.Pass,u.telefono,u.direccion,u.estado,r.id_rol,r.descripcion from Usuario u
-inner join Rol r on r.id_rol = u.id_rol
+--INSERT INTO Rol(descripcion)
+--VALUES('Supervisor')
+
+
+----consulta para llevar todos los usuario a la cd_usuarios--
+--select u.id_usuario,u.DNI,u.Nombre_completo,u.Email,u.Pass,u.telefono,u.direccion,u.estado,r.id_rol,r.descripcion from Usuario u
+--inner join Rol r on r.id_rol = u.id_rol
 
 
 --procedimientos almacenados para USUARIOS ALTA BAJA Y MODIFICACION--
 
 --Registrar usuario--
 
-SELECT * FROM Usuario
+--SELECT * FROM Usuario
 
 create PROC SP_RegistrarUsuario(
 @DNI varchar(50),
@@ -106,14 +106,14 @@ begin
 
   --prueba EDITAR--
 
-  declare @Respuesta bit
-  declare @mensaje varchar(100)
+  --declare @Respuesta bit
+  --declare @mensaje varchar(100)
 
-  exec SP_EditarUsuario 4, '303030','Javier','vende@gmail.com','123',3794444444,'calle789',2,1,@Respuesta output,@mensaje output 
+  --exec SP_EditarUsuario 4, '303030','Javier','vende@gmail.com','123',3794444444,'calle789',2,1,@Respuesta output,@mensaje output 
 
-  select @Respuesta
-  select @mensaje
-  SELECT * FROM Usuario
+  --select @Respuesta
+  --select @mensaje
+  --SELECT * FROM Usuario
 
 
   ---Eliminar usuario--
